2e0da2c06a2fad08c427001e9092b60c381ef574
Corentin DIARD <corentin.diard@gmail.com>
2020-12-20 18:22:17 +0100

    Friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_170429) do
+ActiveRecord::Schema.define(version: 2020_12_20_171600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.boolean "confirmed", default: false
+    t.boolean "refused", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "lastname", default: ""
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_12_20_170429) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "invitations", "users"
 end
